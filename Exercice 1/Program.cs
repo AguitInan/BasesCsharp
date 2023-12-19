654125f6460f585ba395b1592caac7df8eb9f4eb
unknown <aguit.inan@gmail.com>
2023-12-19 19:46:51 +0100

    initial commit

---
+++ Exercice 1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** Ma liste de todo ***");
+            Console.WriteLine("Aujourd'hui je dois faire:");
+            Console.WriteLine("\t-Apprendre le C#");
+            Console.WriteLine("\t-Apprendre à utiliser Visual Studio");
+            Console.WriteLine("\t-Comprendre l'affichage \"Console\"");
+            Console.WriteLine("\t-Créer mon répertoire \"c:\\MesExercices\\\" pour les ranger");
+            Console.WriteLine("\t-Apprécier les fonctionnalités du C#\n\n\n");
+            Console.WriteLine("Appuyez sur une touche pour continuer...");
+            Console.Read();
+        }
+    }
+}
