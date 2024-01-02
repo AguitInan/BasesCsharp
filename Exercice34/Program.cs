8ad3b9f8812ae9ecf7c42b60707dbb093d94b1d2
unknown <aguit.inan@gmail.com>
2024-01-02 18:02:35 +0100

    docs: add Exercice 36

--- Exercice34/Program.cs
+++ Exercice34/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] tab = new int[0];
+Random rnd = new Random();
+
+Console.WriteLine("--- Est pair...? Est impair...? ---");
+Console.Write("Combiens de valeurs contiendra le tableau ? : ");
+if (int.TryParse(Console.ReadLine(), out int nombre))
+{
+    tab = new int[nombre];
+}
+
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Write("\tErreur de saisie, merci de saisir un chiffre/nombre : ");
+    Console.ForegroundColor = ConsoleColor.White;
+}
+
+
+Console.WriteLine("Affectation automatique des valeurs...\n");
+for (int i = 0; i < tab.Length; i++)
+{
+    tab[i] = rnd.Next(1, 51);
+}
+
+
+Console.WriteLine("Vérification des valeurs du tableau : ");
+for (int i = 0; i < tab.Length; i++)
+{
+    if (tab[i] % 2 == 0)
+        Console.WriteLine("\tLa valeur " + tab[i] + " est pair.");
+    else
+        Console.WriteLine("\tLa valeur " + tab[i] + " est impair.");
+}
